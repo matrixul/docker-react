d037148132b91b8db4ec2eae96d38f06547866a1
matrixul <59313583+matrixul@users.noreply.github.com>
2020-05-27 21:57:11 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          = Learn React like powerfull BOSS!!! =
         </a>
       </header>
     </div>
